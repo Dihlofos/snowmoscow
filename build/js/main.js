5e142fa7b4c4900df2d2a10395a1f7a5a078fe07
Evgeny Samsonov <samsonov@aeroidea.ru>
2022-01-03 18:23:40 +0300

    done mobile version

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,50 @@
 "use strict";
 (function () {
+  const container = document.querySelector(".js-activity");
+  if (!container) return;
+
+  const togglers = container.querySelectorAll(".js-activity-button");
+  const content = container.querySelectorAll(".js-activity-item");
+
+  if (!togglers.length | !content.length) return;
+
+  togglers.forEach((toggler) => {
+    toggler.addEventListener("click", (event) => {
+      const target = event.currentTarget;
+      if (!target) return;
+      hideContent();
+      const { id } = target.dataset;
+      target.classList.add("active");
+      container
+        .querySelector(`.js-activity-item[data-item="${id}"]`)
+        .classList.add("active");
+    });
+  });
+
+  function hideContent() {
+    container.querySelectorAll(".active").forEach((activeEl) => {
+      activeEl.classList.remove("active");
+    });
+  }
+
+  // Accordeon
+  const accordeonTogglers = document.querySelectorAll(".js-activity-toggler");
+  if (!accordeonTogglers.length) return;
+
+  accordeonTogglers.forEach((toggler) => {
+    toggler.addEventListener("click", (event) => {
+      const target = event.currentTarget;
+      if (!target) return;
+      const content = target.nextElementSibling;
+      if (!content) return;
+      target.classList.toggle("active");
+      content.classList.toggle("active");
+    });
+  });
+})();
+
+"use strict";
+(function () {
   const togglers = document.querySelectorAll(".js-faq-toggler");
   if (!togglers.length) return;
 
